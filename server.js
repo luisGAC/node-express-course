2e4921bea87e82b7ffbc219b4eb7879a19d4b18f
Luis Gerardo Aguirre Cervantes <lgaguirre@unitedvirtualities.com>
2020-02-18 16:54:58 -0700

    add dynamic GET route

--- server.js
+++ server.js
@@ -16,3 +16,12 @@ app.get('/users', function(req,res){
 app.listen(8000,function(){
      console.log("Server is runnign")
 })
+
+app.get('/users/:id',function(req,res){
+	console.log(req.params.id)
+	res.json({
+		success: true,
+		message: 'got one user',
+		user: req.params.id
+	})
+})
